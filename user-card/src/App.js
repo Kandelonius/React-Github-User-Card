b02ca5dbfdb3386051827c92987168920dafbb7c
Shane Kaestner <shane.kaestner@yahoo.com>
2020-05-05 14:33:25 -0700

    getting data from github

--- user-card/src/App.js
+++ user-card/src/App.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React from "react";
+import axios from "axios";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+const url = 'https://api.github.com/users/Kandelonius';
+
+class App extends React.Component {
+  constructor() {
+    super();
+    console.log("constructor")
+    this.state = {
+      gitUser: ""
+    };
+  }
+  componentDidMount() {
+    axios
+      .get(url)
+      .then(res => {
+        console.log(res);
+      })
+      .catch(err =>
+        console.log(err)
+      );
+  }
+
+  render() {
+    return (
+      <div className="App">
+      </div>
+    );
+  }
 }
 
 export default App;
